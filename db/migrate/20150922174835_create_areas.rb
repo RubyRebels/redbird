00add052788970d6b5ba5837c8873d08998339b2
lvela <louis@trueability.com>
2015-09-24 19:13:32 -0500

    Make area name required.

--- db/migrate/20150922174835_create_areas.rb
+++ db/migrate/20150922174835_create_areas.rb
@@ -1,7 +1,7 @@
 class CreateAreas < ActiveRecord::Migration
   def change
     create_table :areas do |t|
-      t.string :name
+      t.string :name, required: true
       t.references :address, index: true, foreign_key: true
 
       t.timestamps null: false
